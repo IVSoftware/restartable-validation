3c5d62af1d404652cdf76adbf7ecf6542c3ba773
IVSoftware <tgregor@ivsoftware.com>
2020-06-07 14:11:13 -0600

    Add project files.

---
+++ restartable-validation/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace async_delete_many_files
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonStartOrRestart = new System.Windows.Forms.Button();
+            this.textBoxRemaining = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonStartOrRestart
+            // 
+            this.buttonStartOrRestart.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonStartOrRestart.Location = new System.Drawing.Point(12, 25);
+            this.buttonStartOrRestart.Name = "buttonStartOrRestart";
+            this.buttonStartOrRestart.Size = new System.Drawing.Size(260, 23);
+            this.buttonStartOrRestart.TabIndex = 0;
+            this.buttonStartOrRestart.Text = "START OR RESTART VALIDATION";
+            this.buttonStartOrRestart.UseVisualStyleBackColor = true;
+            // 
+            // textBoxRemaining
+            // 
+            this.textBoxRemaining.Location = new System.Drawing.Point(169, 58);
+            this.textBoxRemaining.Name = "textBoxRemaining";
+            this.textBoxRemaining.ReadOnly = true;
+            this.textBoxRemaining.Size = new System.Drawing.Size(100, 20);
+            this.textBoxRemaining.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(105, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Progress";
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Enabled = false;
+            this.buttonCancel.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCancel.Location = new System.Drawing.Point(169, 84);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(100, 23);
+            this.buttonCancel.TabIndex = 0;
+            this.buttonCancel.Text = "CANCEL";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 111);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxRemaining);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonStartOrRestart);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStartOrRestart;
+        private System.Windows.Forms.TextBox textBoxRemaining;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonCancel;
+    }
+}
+
---
+++ restartable-validation/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace async_delete_many_files
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            buttonStartOrRestart.Click += ButtonStartOrRestart_Click;
+            buttonCancel.Click += ButtonCancel_Click;
+            Progress += TaskNotify_Progress;
+            _progress = 0;
+            textBoxRemaining.Text = "0%";
+        }
+        private void ButtonCancel_Click(object sender, EventArgs e)
+        {
+            if(_cts != null) _cts.Cancel();
+            _restart = false;
+            MessageBox.Show("Cancelled");
+            BeginInvoke((MethodInvoker)delegate
+            {
+                buttonCancel.Enabled = false;
+            });
+        }
+        int _progress;
+        bool _restart = false;
+        private void ButtonStartOrRestart_Click(object sender, EventArgs e)
+        {
+            if (_cts != null) _cts.Cancel();
+            try
+            {
+                ssBusy.Wait();
+                switch(ssBusy.CurrentCount)
+                {
+                    case 1:
+                        // Start 
+                        buttonCancel.Enabled = true;
+                        ExecValidityCheck();
+                        break;
+                    case 0:
+                        // Restart
+                        _restart = true;
+                        break;
+                }
+            }
+            finally
+            {
+                ssBusy.Release();
+            }
+        }
+        CancellationTokenSource _cts = null;
+        SemaphoreSlim ssBusy = new SemaphoreSlim(2);
+        private void ExecValidityCheck()
+        {
+            ssBusy.Wait();
+            Task.Run(() =>
+            {
+                try
+                {
+                    _cts = new CancellationTokenSource();
+                    LongRunningValidation(_cts.Token);
+                }
+                finally
+                {
+                    ssBusy.Release();
+                }
+            })
+            .GetAwaiter()
+            .OnCompleted(CheckForRestart);
+        }
+        void CheckForRestart()
+        {
+            BeginInvoke((MethodInvoker)delegate
+            {
+                if (_restart)
+                {
+                    _restart = false;
+                    ExecValidityCheck();
+                }
+                else
+                {
+                    buttonCancel.Enabled = false;
+                }
+            });
+        }
+        private void LongRunningValidation(CancellationToken ct)
+        {
+            _progress = 0;
+            // Run some portion of validity code then check for cancellation.
+            if (CheckForRestart(ct)) return;
+            RunPartialValidityTest();   // i.e. some code {...}
+            // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+            if (CheckForRestart(ct)) return;
+            RunPartialValidityTest();
+            if (CheckForRestart(ct)) return;
+            RunPartialValidityTest();
+            if (CheckForRestart(ct)) return;
+            RunPartialValidityTest();
+            if (CheckForRestart(ct)) return;
+            RunPartialValidityTest();
+            if (CheckForRestart(ct)) return;
+            RunPartialValidityTest();
+            if (CheckForRestart(ct)) return;
+            RunPartialValidityTest();
+            if (CheckForRestart(ct)) return;
+            RunPartialValidityTest();
+            if (CheckForRestart(ct)) return;
+            RunPartialValidityTest();
+            if (CheckForRestart(ct)) return;
+            RunPartialValidityTest();
+        }
+
+        private bool CheckForRestart(CancellationToken ct)
+        {
+            return ct.IsCancellationRequested;
+        }
+
+        private void RunPartialValidityTest()
+        { 
+            // Send progress notifications to UI thread
+            if (_progress == 0) Progress?.Invoke(this, EventArgs.Empty);
+
+            // Simulate running a chunk of validation code
+            Task.Delay(500).Wait();
+            // SIMULATE ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+            // Increment the progress count and notify UI
+            _progress++;
+            Progress?.Invoke(this, EventArgs.Empty);
+        }
+
+        event EventHandler Progress;
+
+        private void TaskNotify_Progress(object sender, EventArgs e)
+        {
+            BeginInvoke((MethodInvoker)delegate 
+            {
+                textBoxRemaining.Text = (_progress * 10).ToString() + "%"; 
+            });
+        }
+    }
+}
